3e76ea86172239aadefffe927ef061ccf93b6ee6
shamilanazIT-QA <shamilanaz9@gmail.com>
2026-05-31 16:03:41 -0500

    Rollback to 'localhost\\SQLEXPRESS'

--- features/environment.py
+++ features/environment.py
@@ -3,16 +3,15 @@ import urllib
 from sqlalchemy import create_engine
 
 def before_all(context):
-    # Check if we are running in the cloud or on your local Beelink machine
+    # If running on GitHub, connect to localhost\SQLEXPRESS with Windows Auth.
+    # Otherwise, connect to your Beelink machine!
     if os.getenv('GITHUB_ACTIONS') == 'true':
-        server = 'localhost'  # Matches the GitHub Runner default server address
-        username = 'sa'
-        # The Windows image comes preconfigured with Windows Auth enabled for the current admin user
+        server = 'localhost\\SQLEXPRESS'
         raw_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE=Bussiness_DB;Trusted_Connection=yes;Encrypt=no;"
     else:
         server = 'Beelink\\SQLEXPRESS'
         username = 'auto_user'
-        password = 'Password123!'  # Replace with your actual local auto_user password
+        password = 'Password123!' # Your local auto_user password
         raw_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE=Bussiness_DB;UID={username};PWD={password};Encrypt=no;TrustServerCertificate=yes;"
 
     params = urllib.parse.quote_plus(raw_string)
