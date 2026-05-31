d1857e4da33da1bf6f86eb8eff97debb44ce27da
shamilanazIT-QA <shamilanaz9@gmail.com>
2026-05-31 15:28:11 -0500

    Updated BD Name.

--- features/environment.py
+++ features/environment.py
@@ -3,22 +3,17 @@ import urllib
 from sqlalchemy import create_engine
 
 def before_all(context):
-    # If running on GitHub, use localhost. Otherwise, use your local Beelink machine!
+    # Check if we are running in the cloud or on your local Beelink machine
     if os.getenv('GITHUB_ACTIONS') == 'true':
-        server = 'localhost\\SQLEXPRESS'
+        server = 'localhost'  # Matches the GitHub Runner default server address
         username = 'sa'
-        password = 'Malik_123'  # Default Windows Runner SA Password container password
+        # The Windows image comes preconfigured with Windows Auth enabled for the current admin user
+        raw_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE=Bussiness_DB;Trusted_Connection=yes;Encrypt=no;"
     else:
         server = 'Beelink\\SQLEXPRESS'
         username = 'auto_user'
-        password = 'Password123!'
+        password = 'Password123!'  # Replace with your actual local auto_user password
+        raw_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE=Bussiness_DB;UID={username};PWD={password};Encrypt=no;TrustServerCertificate=yes;"
 
-    database = 'Bussiness_DB'
-    driver = '{ODBC Driver 17 for SQL Server}'
-
-    raw_string = (
-        f"DRIVER={driver};SERVER={server};DATABASE={database};"
-        f"UID={username};PWD={password};Encrypt=no;TrustServerCertificate=yes;"
-    )
     params = urllib.parse.quote_plus(raw_string)
     context.engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
